f0f463447ee18ad4463603f0f487cf9cb5fcf70f
dianbny <pratama.dianbny@gmail.com>
2024-09-07 06:09:04 +0800

    Menambah Required pada Form Input

--- pages/_detail_activity.php
+++ pages/_detail_activity.php
@@ -12,13 +12,13 @@
     <h5><i class="fa fa-pencil" aria-hidden="true"></i> &nbsp; Form Update Activity</h5><br>
     <form method="POST" action="update-activity-<?= $id; ?>" class="form-input">
         <label for="date">Date &nbsp;<span style="color:red;font-size:15px;"></span></label><br>
-        <input type="date" name="date" value="<?= $dataActivity['_tanggal']; ?>"><br>
+        <input type="date" name="date" value="<?= $dataActivity['_tanggal']; ?>" required><br>
 
         <label for="activity">Activity &nbsp;<span style="color:red;font-size:15px;"></span></label><br>
-        <textarea name="activity" class="textarea" placeholder="Add New Activity"><?= $dataActivity['_aktifitas']; ?></textarea>
+        <textarea name="activity" class="textarea" placeholder="Add New Activity" required><?= $dataActivity['_aktifitas']; ?></textarea>
 
         <label for="status">Status &nbsp;<span style="color:red;font-size:15px;"></span></label><br>
-        <select name="status" class="select">
+        <select name="status" class="select" required>
             <option value="<?= $dataActivity['_status']; ?>" selected><?= $dataActivity['_status']; ?></option>
             <option value="">- Select Status -</option>
             <option value="Done">Done</option>
--- pages/_detail_to_do_list.php
+++ pages/_detail_to_do_list.php
@@ -12,13 +12,13 @@
     <h5><i class="fa fa-pencil" aria-hidden="true"></i> &nbsp; Form Update Activity</h5><br>
     <form method="POST" action="update-to-do-list-<?= $id; ?>" class="form-input">
         <label for="date">Date &nbsp;<span style="color:red;font-size:15px;"></span></label><br>
-        <input type="date" name="date" value="<?= $dataTDL['_tanggal']; ?>"><br>
+        <input type="date" name="date" value="<?= $dataTDL['_tanggal']; ?>" required><br>
 
         <label for="activity">Activity &nbsp;<span style="color:red;font-size:15px;"></span></label><br>
-        <textarea name="activity" class="textarea" placeholder="Add New Activity"><?= $dataTDL['_plan']; ?></textarea>
+        <textarea name="activity" class="textarea" placeholder="Add New Activity" required><?= $dataTDL['_plan']; ?></textarea>
 
         <label for="status">Status &nbsp;<span style="color:red;font-size:15px;"></span></label><br>
-        <select name="status" class="select">
+        <select name="status" class="select" required>
             <option value="">- Select Status -</option>
             <option value="Done">Done</option>
             <option value="Waiting">Waiting</option>
