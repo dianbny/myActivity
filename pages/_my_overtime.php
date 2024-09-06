6c13e327fdcd3568d33f348e247cff37df6c4107
dianbny <pratama.dianbny@gmail.com>
2024-09-07 06:07:38 +0800

    Manambahkan Halaman My Overtime

---
+++ pages/_my_overtime.php
@@ -0,0 +1,19 @@
+<?php
+    if(!isset($_SESSION['status']) || $dataUserLogin['_level'] != "user"){ 
+        header('location:logout');
+    }
+
+    $bulan;
+    $tahun;
+
+    if(isset($_POST['search'])){
+        $bulan = $_POST['month'];
+        $tahun = $_POST['year'];
+    }
+    else {
+        $bulan = date('m');
+        $tahun = date('Y');
+    }
+?>
+
+<label class="labelTitle"><i class="fa fa-angle-double-right" aria-hidden="true"></i>&nbsp;Overtime</label>
