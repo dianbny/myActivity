fd828e6c45f933ae5276e851dc740bf87fa33009
dianbny <pratama.dianbny@gmail.com>
2024-09-11 16:43:22 +0800

    Menambahkan Kolom Tipe Aktifitas, Follow Up dan Informasi Tambahan

--- pages/_daily_activity.php
+++ pages/_daily_activity.php
@@ -22,48 +22,65 @@
         From : <input type="date" name="start" value="<?= $start; ?>"> &nbsp; To : <input type="date" name="end" value="<?= $end; ?>" min="<?= date('Y-m-d'); ?>">
         <input type="submit" name="search" value="Search">
     </form>
-        <?php
-             if($getData->cekDAFromTo($dataUser['_id_pekerja'], $start, $end) > 0){
-        ?>
+
         <span style="font-size:12px;">Daily Activity | Status :&nbsp; <i class="fa fa-circle" aria-hidden="true" style="color:green"></i>&nbsp; Done, <i class="fa fa-circle" aria-hidden="true" style="color:darkorange"></i>&nbsp; Pending </span>
         <div class="table-layout">
             <table class="table-style">
                 <tr>
                     <th>No.</th>
                     <th>Date</th>
+                    <th>Type of Activity</th>
                     <th>Activity</th>
                     <th style="text-align:center;">Status</th>
+                    <th style="text-align:center;">Follow Up</th>
+                    <th>Additional Information</th>
                     <th colspan="2" style="text-align:center;">Action</th>
                 </tr>
                 <?php
-                    $no = 1;
-                    foreach($getData->ListDAFromTo($dataUser['_id_pekerja'], $start, $end) as $row){ ?>
+                    if($getData->cekDAFromTo($dataUser['_id_pekerja'], $start, $end) > 0){
+                        $no = 1;
+                        foreach($getData->ListDAFromTo($dataUser['_id_pekerja'], $start, $end) as $row){ ?>
+                            <tr>
+                                <td><?= $no++."."; ?></td>
+                                <td><?= strftime('%d %B %Y', strtotime($row['_tanggal'])); ?></td>
+                                <td><?= $row['_tipe_aktifitas']; ?></td>
+                                <td><?= $row['_aktifitas']; ?></td>
+                                <td style="text-align:center;">
+                                    <?php
+                                        if($row['_status'] == "Done"){ ?>
+                                            <span style="color:green;font-size:14px;"><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <?php }
+                                        else{ ?>
+                                            <span style="color:darkorange;font-size:14px;"><i class="fa fa-circle" aria-hidden="true"></i></span>
+                                <?php }
+                                    ?>
+                                </td>
+                                <td style="text-align:center;">
+                                    <?php
+                                        if($row['_status'] == "Pending"){ ?>
+                                            <a href="follow-up-activity-<?= $row['_id_aktifitas']; ?>" class="linkDetail"><i class="fa fa-cogs" aria-hidden="true"></i></a>
+                                  <?php }
+                                        else { ?>
+                                            <span style="color:green;font-size:14px;"><i class="fa fa-check" aria-hidden="true"></i>
+                                            </span>
+                                  <?php }
+                                    ?>
+                                </td>
+                                <td><?= $row['_keterangan']; ?></td>
+                                <td style="text-align:center;"><a href="detail-activity-<?= $row['_id_aktifitas']; ?>" class="linkDetail"><i class="fa fa-info-circle" aria-hidden="true"></i></a></td>
+                                <td style="text-align:center;"><a href="javascript:void(0)" data-id="<?= $row['_id_aktifitas']; ?>" class="linkError konfirmDeleteActivity"><i class="fa fa-times-circle" aria-hidden="true"></i></a></td>
+                            </tr>
+                <?php }
+                   
+                    }
+                    else { ?>
                         <tr>
-                            <td><?= $no++."."; ?></td>
-                            <td><?= strftime('%d %B %Y', strtotime($row['_tanggal'])); ?></td>
-                            <td><?= $row['_aktifitas']; ?></td>
-                            <td style="text-align:center;">
-                                <?php
-                                    if($row['_status'] == "Done"){ ?>
-                                        <span style="color:green;font-size:14px;"><i class="fa fa-circle" aria-hidden="true"></i></span>
-                              <?php }
-                                    else{ ?>
-                                        <span style="color:darkorange;font-size:14px;"><i class="fa fa-circle" aria-hidden="true"></i></span>
-                            <?php }
-                                ?>
-                            </td>
-                            <td style="text-align:center;"><a href="detail-activity-<?= $row['_id_aktifitas']; ?>" class="linkDetail"><i class="fa fa-info-circle" aria-hidden="true"></i></a></td>
-                            <td style="text-align:center;"><a href="javascript:void(0)" data-id="<?= $row['_id_aktifitas']; ?>" class="linkError konfirmDeleteActivity"><i class="fa fa-times-circle" aria-hidden="true"></i></a></td>
-                        </tr>
-              <?php }    
+                            <td colspan="6" style="color:red;text-align:center;">Data Activity Not Found !</td>
+                        </tr>    
+             <?php  }    
                 ?>
             </table>
         </div>
-<?php }
-      else { ?>
-        <br>
-        <span style="color:red;font-size:14px;">Data Not Found !</span><br>
-<?php }
-?>
+
 <a href="new-activity" class="linkTransferPg"><i class="fa fa-plus-circle" aria-hidden="true"></i> &nbsp; New Activity</a>
 <a href="daily-activity" class="linkTransferPg"><i class="fa fa-refresh" aria-hidden="true"></i> &nbsp; Refresh</a>
